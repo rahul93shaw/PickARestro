0a90ecca292ef235786c3c08cad7a18256d68658
rahul93shaw <ayush9gupta@gmail.com>
2024-02-28 03:52:56 +0800

    Update WebConfig.java
    Updated web config

--- src/main/java/com/pickarestro/config/WebConfig.java
+++ src/main/java/com/pickarestro/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8889") // Add your Angular UI URL here
+                .allowedOrigins("http://localhost:4200") // Add your Angular UI URL here
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
